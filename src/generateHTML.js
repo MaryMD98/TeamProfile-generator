c2112731ea3a0cbd1aa3a9f73f58c9c090c44b02
Maribel Montes <maribel.doris@gmail.com>
2022-02-01 02:07:01 -0600

    changed file name of layout html and added functions to create the cards of team members

---
+++ src/generateHTML.js
@@ -0,0 +1,15 @@
+// create the layout out of the html page
+
+// create the Manager card
+function renderManager(teamMember){}
+
+// create the Engineer card
+function renderEngineer(teamMember){}
+
+// create the Intern card
+function renderIntern(teamMember){}
+
+//create complete HTML page
+function generateHTML(data){
+    return ``;
+}
