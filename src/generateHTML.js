7c37c72a242db2892b1462950568927efe30ecda
Maribel Montes <maribel.doris@gmail.com>
2022-02-01 03:44:51 -0600

    fixed generate html to save strings for multiple cards

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,30 +1,41 @@
 // create the layout out of the html page
-
+let cards = "";
 // create the Manager card
+// .getRole(), .getName(), .getId(), .getEmail(), .getOfficenum(), 
 function renderManager(teamMember){
-    return ` this is in manager `;
+    
+    return ` this is in  manager ${teamMember.getName()} `;
 }
 
 // create the Engineer card
+// .getRole(), .getName(), .getId(), .getEmail(), .getGithub(), 
 function renderEngineer(teamMember){
-    return ` this is in engineer `;
+    return ` this is in engineer ${teamMember.getEmail()}`;
 }
 
 // create the Intern card
+// .getRole(), .getName(), .getId(), .getEmail(), .getSchool() 
 function renderIntern(teamMember){
-    return ` this is in intern `;
+    return ` this is in intern ${teamMember.getSchool()}`;
 }
 
 //create complete HTML page
 function generateHTML(data){
     // data[i].getRole(), data[i].getName(), data[i].getId(), data[i].getEmail() 
-    //     
-    for(var i=0; i < data.length; i++){
-        if(data[i].getRole() === "Manager"){    
-        }
+    //   getOfficenum(), getGithub(), getSchool()  
+    for(let i=0; i < data.length; i++){
+        if(data[i].getRole() === "Manager"){ 
+            cards = cards.concat(renderManager(data[i]));  
+        } 
+        if(data[i].getRole() === "Engineer"){ 
+            cards = cards.concat(renderEngineer(data[i]));   
+        } 
+        if(data[i].getRole() === "Intern"){ 
+            cards = cards.concat(renderIntern(data[i]));   
+        } 
     }
-
-    return `yes i work, i am returning from generate html`;
+    console.log(cards);
+    return `yes i work, here is what i gathered ${cards} i am returning from generate html`;
 }
 
 module.exports = generateHTML;
